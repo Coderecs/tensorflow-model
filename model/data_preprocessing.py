2f46581481a7ea43a07f9d137fef9e6a58b282f7
uttammittal02 <hemanthmittal02@gmail.com>
2022-10-28 23:04:33 +0530

    preprocessing file ready

--- model/data_preprocessing.py
+++ model/data_preprocessing.py
@@ -2,20 +2,44 @@ import pandas as pd
 import numpy as np
 
 submissions_path = './/submissions.csv'
-
+ratings_path = './/ratings.csv'
 df = pd.read_csv(submissions_path)
 
 
 # Remove irrelevant columns from the dataset
 df.drop(['contestId', 'index', 'COMPILATION_ERROR', 'SKIPPED', 'TESTING','PRESENTATION_ERROR','FAILED','CRASHED','CHALLENGED','REJECTED','PARTIAL','SECURITY_VIOLATED','INPUT_PREPARATION_CRASHED','SUBMITTED'], axis = 1, inplace=True)
 
-'''
-The processing is yet to be written
-'''
+users = np.array(df['username'].unique())
 
-
+errors = ['WRONG_ANSWER', 'TIME_LIMIT_EXCEEDED','RUNTIME_ERROR', 'IDLENESS_LIMIT_EXCEEDED', 'MEMORY_LIMIT_EXCEEDED']
+cnt = 0
+for user in users:
+    db = df[df['username'] == user]
+    user_rating = max(800, db['user_rating'])
+    
+    sum_ = sum([sum(db.loc[:, i]) for i in errors])
+    l_ = [(sum_ + 1) / (sum(db.loc[:, i]) + 1) for i in errors]
+    # print(sum_)
+    for i in db.index:
+        problem_rating = db['problem_rating']
+        l = [db.at[i, j] for j in errors]
+        rat = problem_rating / user_rating
+        s = sum(l) + db.at[i, 'OK']
+        rat *= sum([l[i] * l_[i] for i in range(len(l))])
+        rat /= (s * sum(l_))
+        # print(i)
+        cnt += 1
+        rat = min(rat, 1)
+        rat *= 1000
+        rat = max(rat, 100)
+        rat /= 20
+        df.at[i, 'rating'] = np.int8(round(rat))
+    if (cnt % 100000 == 0):
+        print(cnt)
+    # print(f"User {user} has been processed.")
+    
 # Removing the used columns
 df.drop(['OK', 'WRONG_ANSWER', 'TIME_LIMIT_EXCEEDED', 'RUNTIME_ERROR', 'MEMORY_LIMIT_EXCEEDED', 'IDLENESS_LIMIT_EXCEEDED'], axis=1, inplace=True)
 
 # Saving the dataframe to csv
-df.to_csv('ratings.csv', index=False)
+df.to_csv(ratings_path, index=False)
--- model/research.md
+++ model/research.md
@@ -2,10 +2,16 @@
 
 1. epochs = 20, hiddenDim= 100, learning rate = 0.001, batchSize = 100  -> Not very satisfactory result
 
-2. epochs=10, hiddenDim=500, learningRate=0.01, batchSize=100 -> Not very satisfactory
+2. epochs=20, hiddenDim=500, learningRate=0.01, batchSize=100 -> Not very satisfactory
 
-3. epochs=50, hiddenDim=1000, learningRate=0.01, batchSize=500 -> Some better results I see
+3. epochs=20, hiddenDim=1000, learningRate=0.01, batchSize=500 -> Some better results I see
 
-4. epochs=100, hiddenDim=1000, learningRate=0.01, batchSize=5000 -> too much computationally expensive (itne resources nai h :crycatok::sed:)
+4. epochs=20, hiddenDim=1000, learningRate=0.01, batchSize=5000 -> too much computationally expensive (itne resources nai h :crycatok::sed:)
 
-5. 
+5. epochs=20, hiddenDim=1000, learningRate=0.01, batchSize=1000 -> Nothing much changed from the last one
+
+6. epochs=20, hiddenDim=3000, learningRate=0.01, batchSize=1000 -> Same as before
+
+7. epochs=50, hiddenDim=5000, learningRate=0.1, batchSize=1000 -> Same as before
+
+8. epochs=10, hiddenDim=5000, learningRate=0.1, batchSize=1000 -> Same as before
